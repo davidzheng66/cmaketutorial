0e799540a52bb8df6000d3fdbed29ab1dbbe8dd4
David Zheng <davidzheng66@gmail.com>
2021-01-23 15:43:45 -0500

    add cmake flag define

--- config.sh
+++ config.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#cmake -DUSE_ADDER=ON -S . -B out/build
 cmake -S . -B out/build
 
--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include <adder.hpp>
+// #ifdef USE_ADDER
+	#include <adder.hpp>
+// #endif
 #include <cmaketutorial_config.h>
 
-using namespace std;
-
 int main(int argc, char* argv[]){
-	cout << argv[0] << " Version " << CMAKETUTORIAL_VERSION_MAJOR << "." << CMAKETUTORIAL_VERSION_MINOR << endl;
-	cout << "Hello World!!!" << endl;
-	cout << add(10.0, 20.0) << endl;
+	std::cout << argv[0] << " Version " << CMAKETUTORIAL_VERSION_MAJOR << "." << CMAKETUTORIAL_VERSION_MINOR << std::endl;
+	std::cout << "Hello World!!!" << std::endl;
+#ifdef USE_ADDER
+    std::cout << "Using Adder lib" << std::endl;
+	std::cout << add(10.0, 20.0) << std::endl;
+#else
+	std::cout << "Not Using Adder lib" << std::endl;
+#endif
 }	
